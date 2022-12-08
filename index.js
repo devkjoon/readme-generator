540a504eb54c7e6a7da55e02a40d8a143da0ff0d
Joon Kim <joonkidk@gmail.com>
2022-12-08 08:35:24 -0500

    finished inquirer questions

--- index.js
+++ index.js
@@ -1,2 +1,51 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is your project title?'
+        },
+        {
+            type: 'input', 
+            name: 'description',
+            message: `Let's add a description for your project!`
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What are the installation instructions?'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Enter your usage information!'
+        },
+        {
+            type: 'input',
+            name: 'contributions',
+            message: 'What type of contributions would you like to see?'
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Enter your test instructions!'
+        },
+        {
+            type: 'input',
+            name: 'license',
+            message: 'What license is your project?',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your github username?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email?'
+        }
+    ])
